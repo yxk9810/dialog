6e730bb49274a04b7433728879fd19618930215b
wujindou <wujindou1@163.com>
2023-09-16 13:55:35 +0800

    add evaluate and predict

--- evaluate_and_predict.py
+++ evaluate_and_predict.py
@@ -7,16 +7,16 @@ import argparse
 
 parser = argparse.ArgumentParser(description='命令行中传入一个数字')
 #type是要传入的参数的数据类型  help是该参数的提示信息
-parser.add_argument('model_path', type=str, help='训练后的模型文件')
-parser.add_argument('result_file',type=str,help='结果文件')
+parser.add_argument('--model_path', type=str, help='训练后的模型文件')
+parser.add_argument('--result_file',type=str,help='结果文件')
 args = parser.parse_args()
 print(args)
 from sentence_transformers import SentenceTransformer
-model_name = './finetuned_bge'
+model_name =args.model_path
 model = SentenceTransformer(model_name,device='cuda')
 score = 0.0
 cnt = 0
-with open('dev_bge_data.jsonl','r',encoding='utf-8') as lines:
+with open('/kaggle/input/cail2023/dev_bge_data.jsonl','r',encoding='utf-8') as lines:
   for line in lines:
     cnt+=1
     data = json.loads(line.strip())
@@ -29,3 +29,17 @@ with open('dev_bge_data.jsonl','r',encoding='utf-8') as lines:
     score+=1.0/(sorted_ids.index(0)+1)
 print('evaluate ...'+str(float(score)/cnt))
 
+from tqdm import tqdm
+import json
+res = {}
+data_scores = {}
+with open('/kaggle/input/cail2023/test_cail_data.jsonl','r',encoding='utf-8') as lines:
+  for line in tqdm(lines,total=50):
+    data = json.loads(line.strip())
+    query_emb = model.encode(data['query'], normalize_embeddings=True)
+    doc_emb = model.encode(data['docs'],normalize_embeddings=True)
+    similarity = query_emb@doc_emb.T
+    doc_scores = {doc_id:score for score,doc_id in zip(similarity,data['candidates'])}
+    data_scores[data['idx']] = {'scores':similarity.tolist(),'candidates':data['candidates']}
+
+json.dump(data_scores,open(args.model_path,'w',encoding='utf-8'),ensure_ascii=False,indent=4)
